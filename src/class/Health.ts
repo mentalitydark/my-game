e3e40edb4eeeff036fec95b91a33808353da4e5e
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-06-13 22:43:22 -0300

    create class and interface Health

---
+++ src/class/Health.ts
@@ -0,0 +1,30 @@
+import { IHealth } from "../interface/Health";
+
+export class Health implements IHealth {
+  private _max: number
+  private _current: number
+
+  public constructor(value: number) {
+    this._max = value
+    this._current = value
+  }
+
+  get current(): number { return this._current }
+  get max(): number { return this._max }
+  public isAlive(): boolean { return this._current > 0 }
+  
+  public heal(amount: number) {
+    this._current += amount
+
+    if (this._current > this._max)
+      this._current = this._max
+  }
+
+  public takeDamage(amount: number) {
+    this._current -= amount
+  }
+
+  public increaseMax(amount: number) {
+    this._max += amount
+  };
+}
---
+++ src/interface/Health.ts
@@ -0,0 +1,8 @@
+export interface IHealth {
+  max: number
+  current: number
+  heal: (amount: number) => void
+  takeDamage: (amount: number) => void
+  isAlive: () => boolean
+  increaseMax: (amount: number) => void
+}
