a23f10250e1b7450328e626adbe5bd13f278a0c6
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2024-06-13 22:43:52 -0300

    create class Position

---
+++ src/class/Position.ts
@@ -0,0 +1,17 @@
+export class Position {
+  private _x: number
+  private _y: number
+
+  public constructor(x: number, y: number) {
+    this._x = x
+    this._y = y
+  }
+
+  get x(): number { return this._x }
+  get y(): number { return this._y }
+
+  public update(x: number, y: number) {
+    this._x = x
+    this._y = y
+  }
+}
